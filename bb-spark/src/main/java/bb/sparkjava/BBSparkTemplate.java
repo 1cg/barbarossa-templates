9800bb22921c3353f71d81b6dafd125254118ecb
eddyim <edwardim@berkeley.edu>
2017-07-17 11:54:17 -0700

    Better solution for raw()

--- bb-spark/src/main/java/bb/sparkjava/BBSparkTemplate.java
+++ bb-spark/src/main/java/bb/sparkjava/BBSparkTemplate.java
@@ -11,19 +11,7 @@ public class BBSparkTemplate extends BaseBBTemplate {
     private static ThreadLocal<Request> REQUEST = new ThreadLocal<Request>();
     private static ThreadLocal<Response> RESPONSE = new ThreadLocal<Response>();
 
-    class RawObject {
-        Object in;
-
-        RawObject(Object in) {
-            this.in = in;
-        }
-        public String toString() {
-            return in.toString();
-        }
-        public Object getInstance() {
-            return in;
-        }
-    }
+    private boolean isRaw = false;
 
     public Response getResponse() {
         return RESPONSE.get();
@@ -46,18 +34,16 @@ public class BBSparkTemplate extends BaseBBTemplate {
 
     @Override
     public String toS(Object o) {
-        if (o == null) {
-            return "";
+        if (isRaw) {
+            isRaw = false;
+            return super.toS(o);
         }
-        if (o.getClass() == RawObject.class) {
-            RawObject thisO = (RawObject) o;
-            return super.toS(thisO.getInstance());
-        }
-        return o.toString().replaceAll("&", "&amp;").replaceAll("<", "&lt;").replaceAll(">", "&gt;")
+        return super.toS(o).replaceAll("&", "&amp;").replaceAll("<", "&lt;").replaceAll(">", "&gt;")
                 .replaceAll("\"", "&quot").replaceAll("'", "&#39");
     }
 
     public Object raw(Object o) {
-        return new RawObject(o);
+        isRaw = true;
+        return o;
     }
 }
