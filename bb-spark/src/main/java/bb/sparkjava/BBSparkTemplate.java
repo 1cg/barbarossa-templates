5054a43234d24396f2ed1984616e8ff101327e7e
eddyim <edwardim@berkeley.edu>
2017-07-17 11:50:31 -0700

    Supported raw()

--- bb-spark/src/main/java/bb/sparkjava/BBSparkTemplate.java
+++ bb-spark/src/main/java/bb/sparkjava/BBSparkTemplate.java
@@ -11,6 +11,19 @@ public class BBSparkTemplate extends BaseBBTemplate {
     private static ThreadLocal<Request> REQUEST = new ThreadLocal<Request>();
     private static ThreadLocal<Response> RESPONSE = new ThreadLocal<Response>();
 
+    class RawObject {
+        Object in;
+
+        RawObject(Object in) {
+            this.in = in;
+        }
+        public String toString() {
+            return in.toString();
+        }
+        public Object getInstance() {
+            return in;
+        }
+    }
 
     public Response getResponse() {
         return RESPONSE.get();
@@ -33,10 +46,18 @@ public class BBSparkTemplate extends BaseBBTemplate {
 
     @Override
     public String toS(Object o) {
-
-        String s = super.toS(o);
-        return s.replaceAll("&", "&amp;").replaceAll("<", "&lt;").replaceAll(">", "&gt;")
+        if (o == null) {
+            return "";
+        }
+        if (o.getClass() == RawObject.class) {
+            RawObject thisO = (RawObject) o;
+            return super.toS(thisO.getInstance());
+        }
+        return o.toString().replaceAll("&", "&amp;").replaceAll("<", "&lt;").replaceAll(">", "&gt;")
                 .replaceAll("\"", "&quot").replaceAll("'", "&#39");
+    }
 
+    public Object raw(Object o) {
+        return new RawObject(o);
     }
 }
