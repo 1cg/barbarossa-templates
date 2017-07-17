e4edcdda63530ab976e3323c8f0d70f5ea221b36
eddyim <edwardim@berkeley.edu>
2017-07-17 14:59:33 -0700

    Added BBTemplates class and some infrastructure

--- bb-templates/src/main/java/bb/BBTemplates.java
+++ bb-templates/src/main/java/bb/BBTemplates.java
@@ -2,6 +2,14 @@ package bb;
 
 /**
  * Created by eim on 7/17/2017.
+ * For configuration purposes
  */
 public class BBTemplates {
+    public static void setDefaultTemplate(Class layout) {
+
+    }
+
+    public static void setDefaultTemplate(String somePackage, Class layout) {
+
+    }
 }
