3f68302ece9d39cbe0f70441d06bbee4d9255cc9
eddyim <edwardim@berkeley.edu>
2017-07-19 10:26:26 -0700

    Added Default Layout

---
+++ bb-templates/src/main/java/bb/runtime/DefaultLayout.java
@@ -0,0 +1,17 @@
+package bb.runtime;
+
+import java.io.IOException;
+
+/**
+ * Created by eim on 7/19/2017.
+ */
+public class DefaultLayout implements ILayout {
+    public void header(Appendable buffer) throws IOException {
+        return;
+    }
+
+    public void footer(Appendable buffer) throws IOException {
+        return;
+    }
+
+}
