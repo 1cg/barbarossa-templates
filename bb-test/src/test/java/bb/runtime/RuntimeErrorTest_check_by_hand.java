f361cae44346e8fdc7389fb9237e0ced9e449761
hkalidhindi <jrharika@gmail.com>
2017-08-08 14:09:30 -0700

    remove runtimeTests from run all tests/make it manual

--- bb-test/src/test/java/bb/runtime/RuntimeErrorTest_check_by_hand.java
+++ bb-test/src/test/java/bb/runtime/RuntimeErrorTest_check_by_hand.java
@@ -6,8 +6,8 @@ import runtime.*;
 /**
  * Created by eim on 8/1/2017.
  */
-//these erros have to be tested by hand and eye-balled
-public class RuntimeErrorTest {
+//these errors have to be tested by hand and eye-balled
+public class RuntimeErrorTest_check_by_hand {
 //    @Test
 //    public void basicErrorWorks() {
 //        BasicRuntimeError.render();
